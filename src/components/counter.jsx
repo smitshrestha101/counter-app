91b22d91c1814ab48b6cbb457a53cf9417829627
Smit Shrestha <smitshrestha101@gmail.com>
2018-12-30 22:24:40 -0600

    added a new component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,16 +6,39 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  // renderTags() {
+  //   if (this.state.tags.length == 0) return <p>There are no tags!</p>;
+
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+
+  handleIncrement = () => {
+    //console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.renderTags()} */}
       </div>
     );
   }
